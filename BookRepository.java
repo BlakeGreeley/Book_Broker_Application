f343064ecf7ac90123807a3338672a8b0ce0f814
Blake Greeley <98073180+BlakeGreeley@users.noreply.github.com>
2022-06-15 20:09:45 -0700

    Add files via upload

---
+++ BookRepository.java
@@ -0,0 +1,7 @@
+package com.codingdojo.bookbroker.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface BookRepository extends CrudRepository {
+
+}
---
+++ UserRepository.java
@@ -0,0 +1,7 @@
+package com.codingdojo.bookbroker.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository{
+
+}
