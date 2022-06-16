07ef2feb86fdd3bc75ee226260cf58dd45e396d4
Blake Greeley <98073180+BlakeGreeley@users.noreply.github.com>
2022-06-16 09:23:18 -0700

    Update UserRepository.java

--- UserRepository.java
+++ UserRepository.java
@@ -1,7 +1,12 @@
 package com.codingdojo.bookbroker.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository{
+import com.codingdojo.bookbroker.models.User;
 
+public interface UserRepository extends CrudRepository <User, Long>{
+	
+	Optional<User> findByEmail(String email);
 }
